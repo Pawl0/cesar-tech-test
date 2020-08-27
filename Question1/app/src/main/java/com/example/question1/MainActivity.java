d3b9d809cf5b11ad6565a24d00e49e548d672a08
Pawl0 <paulohsantosol@gmail.com>
2020-08-26 23:40:28 -0400

    Finished project

---
+++ Question1/app/src/main/java/com/example/question1/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.question1;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import com.example.question1.adapter.Adapter;
+import com.example.question1.model.Fruit;
+import com.example.question1.service.HTTPService;
+
+import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final List<Fruit> listFruits = listAllFruits();
+        final ListView fruitListView = (ListView) findViewById(R.id.quantity_card_fruits);
+        final Adapter adapter = new Adapter(listFruits, this);
+        System.out.println("Lista de frutas: "+listFruits);
+        fruitListView.setAdapter(adapter);
+
+        fruitListView.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Fruit fruit = listFruits.get(position);
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("fruit", fruit);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private List<Fruit> listAllFruits() {
+        HTTPService service = new HTTPService();
+        List<Fruit> aux = new ArrayList();
+        try {
+            aux = service.execute().get();
+            System.out.println("aux: "+aux);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return aux;
+    }
+}
